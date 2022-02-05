25d65cfcb879a6b073ccdfda960bd647a08e9c34
Hal-Zhaodong-Yang <zdyang324@gmail.com>
2022-02-04 23:36:55 -0500

    header file for trifocal tensor

---
+++ gtsam/geometry/TrifocalTensor1d.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <gtsam/base/Manifold.h>
+#include <gtsam/geometry/Point2.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/geometry/Unit3.h>
+
+#include <iosfwd>
+#include <string>
+
+// should I include the macro GTSAM_EXPORT?
+namespace gtsam {
+class TrifocalTensor1D {
+ private:
+ 
+}
+}  // namespace gtsam
